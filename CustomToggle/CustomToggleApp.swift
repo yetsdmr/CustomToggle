f57615f173474fa7b1a2f44c8f4e57706d2718e6
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-13 09:29:46 +0300

    Initial Commit

---
+++ CustomToggle/CustomToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomToggleApp.swift
+//  CustomToggle
+//
+//  Created by Yunus Emre Taşdemir on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
